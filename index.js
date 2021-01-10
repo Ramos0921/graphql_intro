b1aa07fd896315b942c138a97328d3f89d5b2d80
Ramos0921 <sfcc.eduardo.ramos@gmail.com>
2021-01-10 12:55:43 -0700

    schema and query layout set

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 import express from 'express';
+import schema from './schema';
+import { graphqlHTTP } from 'express-graphql'
 
 const app = express();
 
 app.get('/',(req,res)=>{
   res.send('gql')
 });
+
+const root = { friend: ()=> {
+  return{
+    "id":387638767682,
+    "firstName":"Eduardo",
+    "lastName":"Ramos",
+    "gender":"Male",
+    "email":[
+      {email:"me@me.com"},
+      {email:"you@you.com"}
+    ]
+  }
+}};
+
+app.use('/graphql', graphqlHTTP({
+  schema:schema,
+  rootValue:root,
+  graphiql:true,
+}))
 
 app.listen(8080,()=>{
   console.log("server up")
